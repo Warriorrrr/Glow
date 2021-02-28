372afe8afda3f2ab4736091c847f4d1e88e7fc39
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-02-28 20:22:06 +0100

    Final fixes

--- src/main/java/dev/warriorrr/glow/PlayerListener.java
+++ src/main/java/dev/warriorrr/glow/PlayerListener.java
@@ -5,17 +5,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.inventivetalent.glow.GlowAPI;
-import org.inventivetalent.glow.GlowAPI.Color;
 
 public class PlayerListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        if (event.getPlayer().hasPermission("group.premium")) {
-            Bukkit.getScheduler().runTaskLater(Glow.getPlugin(), new Runnable(){
-                public void run() {
-                    GlowAPI.setGlowing(event.getPlayer(), Color.PURPLE, Bukkit.getOnlinePlayers());
-                }
-            }, 5);
-        }
+        if (event.getPlayer().hasPermission("group.premium"))
+            GlowAPI.setGlowing(event.getPlayer(), GlowAPI.Color.PURPLE, Bukkit.getOnlinePlayers());
     }
 }
